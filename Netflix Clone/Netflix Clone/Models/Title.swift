553f99d799e9896b5fffaba4737b24675ee7a99d
Usr_Prime <>
2022-04-19 07:48:57 -0700

    Criação de CollectionViewCell para por Capa dos filmes

--- Netflix Clone/Netflix Clone/Models/Title.swift
+++ Netflix Clone/Netflix Clone/Models/Title.swift
@@ -1,5 +1,5 @@
 //
-//  Tv.swift
+//  Movie.swift
 //  Netflix Clone
 //
 //  Created by Usr_Prime on 19/04/22.
@@ -7,10 +7,10 @@
 
 import Foundation
 
-struct TrendingTvResponse: Codable {
-    let results: [Tv]
+struct TrendingTitleResponse: Codable {
+    let results: [Title]
 }
-struct Tv: Codable {
+struct Title: Codable {
     let id: Int
     let media_type: String?
     let original_name: String?
---
+++ Netflix Clone/Netflix Clone/Views/TitleCollectionViewCell.swift
@@ -0,0 +1,40 @@
+//
+//  TitleCollectionViewCell.swift
+//  Netflix Clone
+//
+//  Created by Usr_Prime on 19/04/22.
+//
+
+import UIKit
+
+class TitleCollectionViewCell: UICollectionViewCell {
+    static let identifier = "TitleCollectionViewCell"
+    
+    private let posterImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        contentView.addSubview(posterImageView)
+    }
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        posterImageView.frame = contentView.bounds
+    }
+    
+    func configureCell(with link: String) {
+        guard let imageURL = URL(string: link) else { return }
+        DispatchQueue.global().async {
+            guard let imageData = try? Data(contentsOf: imageURL) else { return }
+            let imageEnd = UIImage(data: imageData)
+            DispatchQueue.main.async {
+                self.posterImageView.image = imageEnd
+            }
+        }
+    }
+    
+    required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
+}
